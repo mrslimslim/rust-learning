3225d1b1535631a7e47ec60a606c82e27daeb2d0
mrslimslim <2451319596@qq.com>
2022-06-23 23:29:31 +0800

    update

--- rust-waterprint/src/lib.rs
+++ rust-waterprint/src/lib.rs
@@ -4,7 +4,7 @@
  * @Autor: tengyu
  * @Date: 2022-06-22 22:27:48
  * @LastEditors: tengyu
- * @LastEditTime: 2022-06-23 00:50:47
+ * @LastEditTime: 2022-06-23 23:08:58
  */
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -20,46 +20,56 @@ pub fn generate_watermark(anchor: Option<String>) {
                 .query_selector(&node)
                 .expect("I need a node")
                 .unwrap()
-                .dyn_into::<HtmlElement>()
-                .unwrap();
-            watermark(&document, &node);
-            document
-                .append_child(&node)
-                .expect("count append node to document");
+                .dyn_into::<HtmlElement>();
+            match node {
+                Ok(node) => {
+                    watermark(&document, &node);
+                    document
+                        .append_child(&node)
+                        .expect("count append node to document");
+                }
+                Err(e) => {}
+            }
         }
         None => {}
     }
 }
 
-fn watermark(dom: &Document, anchor: &HtmlElement) {
+fn watermark(dom: &Document, anchor: &HtmlElement) -> Result<(), ()> {
     let canvas = dom
         .create_element("canvas")
         .unwrap()
-        .dyn_into::<HtmlCanvasElement>()
-        .unwrap();
-    canvas.set_width(anchor.client_width() as u32);
-    canvas.set_height(anchor.client_height() as u32);
+        .dyn_into::<HtmlCanvasElement>();
 
-    let ctx = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<CanvasRenderingContext2d>()
-        .unwrap();
-    ctx.set_font("24px Times New Roman");
-    ctx.set_fill_style(&"#0000FF".into());
-    ctx.set_text_align("right");
-    ctx.fill_text(
-        "I am waterprint",
-        (canvas.width() - 20) as f64,
-        (canvas.height() - 20) as f64,
-    );
-    let base64 = canvas.to_data_url_with_type("image/png").unwrap();
-    // anchor
-    //     .append_child(&canvas)
-    //     .expect("could not append canvas");
-    anchor.style().set_property(
-        "background-image",
-        &("url(\"".to_string() + &base64 + "\")")[..],
-    );
+    match canvas {
+        Ok(canvas) => {
+            canvas.set_width(anchor.offset_width() as u32);
+            canvas.set_height(anchor.offset_height() as u32);
+            let ctx = canvas
+                .get_context("2d")
+                .expect("bbbbb")
+                .expect("aaaa")
+                .dyn_into::<CanvasRenderingContext2d>()
+                .expect("xxx");
+            ctx.set_font("24px Times New Roman");
+            ctx.set_fill_style(&"#0000FF".into());
+            ctx.set_text_align("right");
+            ctx.fill_text(
+                "I am waterprint",
+                (canvas.width() - 20) as f64,
+                (canvas.height() - 20) as f64,
+            )
+            .expect("xxxxx");
+            let base64 = canvas.to_data_url_with_type("image/png").unwrap();
+            anchor
+                .style()
+                .set_property(
+                    "background-image",
+                    &("url(\"".to_string() + &base64 + "\")")[..],
+                )
+                .expect("yyyyy");
+        }
+        Err(e) => {}
+    }
+    Ok(())
 }
