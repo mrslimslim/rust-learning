f04459f1ffc62ea514c5c18a438e250d02653497
mrslimslim <2451319596@qq.com>
2022-07-31 18:21:24 +0800

    update

--- rust-waterprint/src/lib.rs
+++ rust-waterprint/src/lib.rs
@@ -4,7 +4,7 @@
  * @Autor: tengyu
  * @Date: 2022-06-22 22:27:48
  * @LastEditors: tengyu
- * @LastEditTime: 2022-06-23 23:08:58
+ * @LastEditTime: 2022-06-26 11:33:59
  */
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -39,37 +39,32 @@ fn watermark(dom: &Document, anchor: &HtmlElement) -> Result<(), ()> {
     let canvas = dom
         .create_element("canvas")
         .unwrap()
-        .dyn_into::<HtmlCanvasElement>();
+        .unchecked_into::<HtmlCanvasElement>();
 
-    match canvas {
-        Ok(canvas) => {
-            canvas.set_width(anchor.offset_width() as u32);
-            canvas.set_height(anchor.offset_height() as u32);
-            let ctx = canvas
-                .get_context("2d")
-                .expect("bbbbb")
-                .expect("aaaa")
-                .dyn_into::<CanvasRenderingContext2d>()
-                .expect("xxx");
-            ctx.set_font("24px Times New Roman");
-            ctx.set_fill_style(&"#0000FF".into());
-            ctx.set_text_align("right");
-            ctx.fill_text(
-                "I am waterprint",
-                (canvas.width() - 20) as f64,
-                (canvas.height() - 20) as f64,
-            )
-            .expect("xxxxx");
-            let base64 = canvas.to_data_url_with_type("image/png").unwrap();
-            anchor
-                .style()
-                .set_property(
-                    "background-image",
-                    &("url(\"".to_string() + &base64 + "\")")[..],
-                )
-                .expect("yyyyy");
-        }
-        Err(e) => {}
-    }
+    canvas.set_width(anchor.offset_width() as u32);
+    canvas.set_height(anchor.offset_height() as u32);
+    let ctx = canvas
+        .get_context("2d")
+        .expect("bbbbb")
+        .expect("aaaa")
+        .dyn_into::<CanvasRenderingContext2d>()
+        .expect("xxx");
+    ctx.set_font("24px Times New Roman");
+    ctx.set_fill_style(&"#0000FF".into());
+    ctx.set_text_align("right");
+    ctx.fill_text(
+        "I am waterprint",
+        (canvas.width() - 20) as f64,
+        (canvas.height() - 20) as f64,
+    )
+    .expect("xxxxx");
+    let base64 = canvas.to_data_url_with_type("image/png").unwrap();
+    anchor
+        .style()
+        .set_property(
+            "background-image",
+            &("url(\"".to_string() + &base64 + "\")")[..],
+        )
+        .expect("yyyyy");
     Ok(())
 }
